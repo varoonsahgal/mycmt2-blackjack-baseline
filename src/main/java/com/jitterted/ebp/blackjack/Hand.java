46c77aab4f8c409ef3489a6490028991dfea9d5c
varoonsahgal <varoon.sahgal@gmail.com>
2025-12-19 12:57:06 -0500

    PURIFY HAND CLASS

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -2,7 +2,6 @@ package com.jitterted.ebp.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
@@ -16,7 +15,7 @@ public class Hand {
     public Hand() {
     }
 
-    private int value() {
+    public int value() {
         int handValue = cards
                 .stream()
                 .mapToInt(Card::rankValue)
@@ -35,19 +34,27 @@ public class Hand {
         return handValue;
     }
 
-    String displayFaceUpCard() {
-        return ConsoleCard.display(cards.get(0));
+
+//why do i want to MOVE this method?
+    // because it "knows" about the display - and the Hand class
+    // really should NOT know anything about display in the console - if i want to
+    // play BJ on the web, now this is a problem
+    // Hand should only be concerned with how to represent a HAND in blackjack
+
+
+    //Why do i want to make this method static?  So it easier to move to another
+    // class where it belongs
+
+    public Card dealerFaceUpCard() {
+        return cards.get(0);
     }
 
     boolean dealerMustDrawCard() {
         return value() <= 16;
     }
 
-    void display() {
-        System.out.println(cards.stream()
-                                .map(ConsoleCard::display)
-                                .collect(Collectors.joining(
-                                        ansi().cursorUp(6).cursorRight(1).toString())));
+    public List<Card> cards() {
+        return new ArrayList<>(cards);
     }
 
     public void drawFrom(Deck deck) {
@@ -64,10 +71,6 @@ public class Hand {
 
     boolean beats(Hand hand) {
         return hand.value() < value();
-    }
-
-    String displayValue() {
-        return String.valueOf(value());
     }
 
     public boolean valueEquals(int target) {
